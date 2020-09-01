6615c0a44a0b568830158449a8267fd946519c41
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2020-09-01 21:27:30 +0530

    q2 cpp solution

---
+++ 1. Arrays and Strings/2 Permutatoin of one string in another/Solution.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+using namespace std;
+
+bool checkPalin(string arr1,string arr2)
+{   /*
+    Time Complex : O(n) where n is the length of string
+    Space Complex : O(n) 
+
+    Assumes both strings are of equal length and we are not searching for subscript
+    */
+    if(arr1.length()!=arr2.length())
+        return false;
+    
+    //initialize unordered map
+    unordered_map<int,int> char_map;
+
+    for(auto i:arr1)
+        char_map[i] ++;
+
+    for (auto i :arr2)
+    {
+        if(char_map.find(i)==char_map.end())
+            return false;
+        else if(char_map[i] == 0)
+            return false;
+        else
+            char_map[i] -=1;
+        
+    }
+
+    return true;
+    
+}
+
+int main()
+{
+    string str1 = "abcdef";
+    string str2 = "cbafedd";
+    bool res = checkPalin(str1,str2);
+    if(res)
+        cout<<"yes they are palindrome";
+    else
+    {
+        cout<<"they are not palindrome";
+    }
+
+    return 0;
+    
+}
