54089c83b78345cdee7872d523850120a89c2f19
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2020-09-01 17:56:15 +0530

    Bit vector implementation

--- 1. Arrays and Strings/1_Unique_Characters_in_String.cpp
+++ 1. Arrays and Strings/1_Unique_Characters_in_String.cpp
@@ -2,8 +2,18 @@
 #include<iostream>
 using namespace std;
 
+
+/*
+1. Assumed 128 ASCII characters, Can modify to 256 char in extended ASCII
+2. 1st Check if we are dealing with ASCII or UNICODE
+*/
 bool IsUnique(string str)
-{
+{   /*
+    USING EXTRA DATA STRUCTURE
+
+    Space Complexity  : O(1) where c is the character set (but we are taking 128 space for characters O(c) )
+    Time Complexity   : O(n) WHERE n is length of string
+*/
     int len = str.length();
     //if greater than character limit, then dublicacy is obviosly there
     if(len > 128)
@@ -26,11 +36,45 @@ bool IsUnique(string str)
     return true;
 }
 
+bool IsUniqueBitVector(string str)
+{   /*
+    USING BIT VECTOR : OPTIMIZATION ON SPACE ASPECT!
+
+    time complexity :   O(N)
+    SPACE complexity:   O(1)
+
+    Assume if character set is just from 'a' - 'z'.
+    If more set, then we have to take diffirent data type rather than int
+*/
+    int checker = 0;    //bit vector of int as it has a size of 32 and since we hae 26 characters we can use bit manipulation to set flags
+
+    for(int i=0;i<str.length();i++)
+    {
+        int val = str.at(i) - 'a'; //find shift from initial char 'a' to set that flag at that position
+        if((checker & (1<<val))>0) // shift left to location of character and compare with 0 bit and check if > 0  means checker also has 1 at that bit and thus already present
+            return false;
+        
+        //update checker flag
+        checker |= (1<<val); // we do or here as we want to set/mask bit at that position(val) to 1 :- denoting that that char is present 
+
+    }
+
+    // if completes loop
+    return true;
+}
+
 int main()
 {
     string str = "abcdefghijklmnopqrst";
     bool x = IsUnique(str);
     if(x)
+        cout<<"Everthing is distinct in the string " + str;
+    else
+        cout<<"Not distinct in the string " + str;
+
+    cout<<endl;
+    bool bit = IsUniqueBitVector(str);
+    if(bit)
         cout<<"Everthing is distinct in the string " + str;
     else
         cout<<"Not distinct in the string " + str;
