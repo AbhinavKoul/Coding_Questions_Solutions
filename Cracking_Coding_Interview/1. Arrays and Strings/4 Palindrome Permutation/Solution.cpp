079e21ccf18da73a62471372fdf025480e1df118
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2020-09-04 18:18:35 +0530

    Folder Rearrangement

---
+++ Cracking_Coding_Interview/1. Arrays and Strings/4 Palindrome Permutation/Solution.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#define ul unordered_map
+
+using namespace std;
+
+bool checkOneOdd_inMap(ul<int,int> map)
+{
+    bool found_odd = false;
+    for(auto i:map.)
+    {
+
+    }
+}
+
+bool checkPalindromePossible(string str)
+{
+    int count_odd;
+    ul<int,int> char_counter;
+    for(auto i : str)
+        char_counter[i]++;
+    
+    //check only one odd value in the table
+    result = checkOneOdd_inMap(char_counter);
+    
+
+}
