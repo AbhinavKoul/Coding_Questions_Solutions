d1741c2482fdb3116abce005c531567b7705f4e0
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2020-09-21 22:27:39 +0530

    most effiecient GOOGLE

--- InterViewBit/Binary Search/(GOOGLE)Search for a range OR Count Occurances/Solution.cpp
+++ InterViewBit/Binary Search/(GOOGLE)Search for a range OR Count Occurances/Solution.cpp
@@ -2,6 +2,55 @@
 #include<vector>
 using namespace std;
 
+vector<int> searchRange(const vector<int> &A, int B) {
+    /*
+    Time : O(logN)
+    */
+    int left,right,low,high,mid;
+    left = right = -1;
+    bool found = false;
+    //binary search for rightmost position
+    low = 0;
+    high = A.size() - 1;
+    while(low<=high)
+    {
+        mid = (low+high)/2;
+        if(A[mid] == B)
+        {
+            found = true;
+            right = mid;
+            low = mid + 1;
+        }
+        else if(B < A[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    
+    //binary search for leftmost position
+    low = 0;
+    high = A.size() - 1;
+    while(low<=high)
+    {
+        mid = (low+high)/2;
+        if(A[mid] == B)
+        {
+            found = true;
+            left = mid;
+            high = mid - 1;
+        }
+        else if(B <A[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    // IMPORTANT ------------------------------------- HOW TO RETURN  A VECTOR ---------------------------------------
+    if(found)
+        return(vector<int> {left,right});
+    else
+        return(vector<int> {-1,-1});
+
+}
 
 int findCount_MostEfficient(vector<int> arr,int key)
 {
