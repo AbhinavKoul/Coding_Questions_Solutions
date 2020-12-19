ecdd927601e5c7c6b1830cfb6de9c57f6df8606d
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2020-12-19 17:55:36 +0530

    tree functionalities

--- Implementations/Trees/Trees.cpp
+++ Implementations/Trees/Trees.cpp
@@ -13,6 +13,7 @@ struct node
     node(int val) : data(val),left(nullptr),right(nullptr) { }
 };
 
+
 v<int> Inorder(node * head,v<int>& res)
 {
     if(head!=nullptr)
@@ -46,7 +47,6 @@ v<int> LevelOrderTransvsal_helper(node * head,v<int>& res,queue<node*>& Q)
     return res;
     
 }
-
 void LevelOrderTransvesal(node*head)
 {
     queue<node*> Q;
@@ -56,6 +56,58 @@ void LevelOrderTransvesal(node*head)
     for(auto i: res)
         cout<<i<<" ";
 }
+
+int height(node* head)
+{
+    //base
+
+    // if tree is null
+    if(head == nullptr)
+        return 0;
+    // if last node
+    if(head->left == nullptr && head->right == nullptr)
+        return 1;
+    
+    //normal
+    return (max(height(head->left),height(head->right)) + 1);
+    
+}
+
+void returnSpecificLevelElements_helper(node* head,int level,int currLevel,v<int>& res)
+{
+    //base
+
+    //if 1st level only
+    if(level == 1)
+    {
+        res.push_back(head->data);
+        return;
+    }
+    //if currlevel we are in is same as level asked
+    if(currLevel == level)
+    {
+        res.push_back(head->data);
+        return;
+    }
+
+    //normal
+    if(head->left != nullptr)
+        returnSpecificLevelElements_helper(head->left,level,currLevel + 1,res);
+    if(head->right != nullptr)
+        returnSpecificLevelElements_helper(head->right,level,currLevel + 1,res);
+
+}
+
+void specificLevel(node*head,int level)
+{
+    v<int> res;
+
+    returnSpecificLevelElements_helper(head,level,1,res);
+
+    for(auto i : res)
+        cout<<i<<" ";
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -74,7 +126,8 @@ int main()
     // res = Inorder(root,res);
 
     cout<<" fdsfdsds : \n";
-    LevelOrderTransvesal(root);
+    // LevelOrderTransvesal(root);
+    specificLevel(root,3);
 
     // for(auto i : res)
     //     cout<<i<<" ";
