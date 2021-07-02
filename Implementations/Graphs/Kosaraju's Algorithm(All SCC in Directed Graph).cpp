501ab4a543146c5a8288a79bd8b30d829f2282b8
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2021-07-03 03:00:15 +0530

    major graph updates

---
+++ Implementations/Graphs/Kosaraju's Algorithm(All SCC in Directed Graph).cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<stack>
+using namespace std;
+#define v vector
+// https://www.youtube.com/watch?v=V8qIqJxCioo&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=27
+void dfs(int node, stack<int> &st, vector<int> &vis, vector<v<int>>& adj) {
+    vis[node] = 1; 
+    for(auto it: adj[node]) {
+        if(!vis[it]) {
+            dfs(it, st, vis, adj); 
+        }
+    }
+    
+    st.push(node); 
+}
+void revDfs(int node, vector<int> &vis, vector<v<int>>& transpose) {
+    cout << node << " "; 
+    vis[node] = 1; 
+    for(auto it: transpose[node]) {
+        if(!vis[it]) {
+            revDfs(it, vis, transpose); 
+        }
+    }
+}
+int main() {
+    int n, m;
+    cin >> n >> m; 
+	vector<v<int>> adj(n); 
+	for(int i = 0;i<m;i++) {
+	    int u, v;
+	    cin >> u >> v; 
+	    adj[u].push_back(v);
+	}
+	
+	//Step 1 : Topological sort ( order of finishing time)
+	stack<int> st;
+	vector<int> vis(n, 0); 
+	for(int i = 0;i<n;i++) {
+	    if(!vis[i]) {
+	        dfs(i, st, vis, adj); 
+	    }
+	} 
+	
+	//step 2 : topological sorting
+	vector<v<int>> transpose(n); 
+	
+	for(int i = 0;i<n;i++) {
+	    vis[i] = 0; 
+	    for(auto it: adj[i]) {
+	        transpose[it].push_back(i); 
+	    }
+	}
+	
+	
+	//step 3 : DFS ACCORDING TO FINISHING TIME in transpose arrray
+	while(!st.empty()) {
+	    int node = st.top();
+	    st.pop(); 
+	    if(!vis[node]) {
+	        cout << "SCC: "; 
+	        revDfs(node, vis, transpose); 
+	        cout << endl; 
+	    }
+	}
+	
+	
+	return 0;
+}
+
+/*
+5 5 
+0 1 
+1 2 
+2 0 
+1 3 
+3 4 
+*/
