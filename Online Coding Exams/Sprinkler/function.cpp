ab11fefada471ab0976f90f42b9b379939198a62
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2021-08-29 00:02:18 +0530

    mata

--- Online Coding Exams/Sprinkler/function.cpp
+++ Online Coding Exams/Sprinkler/function.cpp
@@ -8,11 +8,29 @@
 using namespace std;
 #define v vector
 #define ll long long 
-int solve()
-{
-    
+
+// typedef long long ll;
+
+ll findValue(int n,int arr[]){
+    long long ans=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=0;k<=1e3;k++){
+                int val1 = pow(2,k)*(((arr[i]*arr[j])/(int)pow(2,2*k))%2);
+                ans += (val1*val2);
+                // cout<<val1<<" "<<val2<<endl;
+                if(arr[i]/(int)pow(2,k)==0 && arr[j]/(int)pow(2,k) ==0)
+                    break;
+            }
+        }
+    }
+
+    return ans;
 }
-int main()
-{
+
+int main(){
+    int n=2;
+    int arr[]={11,39};
+    cout<<findValue(n,arr);
     return 0;
 }
