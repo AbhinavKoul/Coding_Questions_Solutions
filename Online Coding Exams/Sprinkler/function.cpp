3e60a5f6ab0dd171a165e45b737f640bdac2f805
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2021-08-29 00:08:23 +0530

    mata final

--- Online Coding Exams/Sprinkler/function.cpp
+++ Online Coding Exams/Sprinkler/function.cpp
@@ -5,19 +5,21 @@
 #include<unordered_map>
 #include<string>
 #include<algorithm>
+#include<set>
 using namespace std;
 #define v vector
-#define ll long long 
+using namespace std;
 
-// typedef long long ll;
+typedef long long ll;
 
 ll findValue(int n,int arr[]){
     long long ans=0;
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             for(int k=0;k<=1e3;k++){
-                int val1 = pow(2,k)*(((arr[i]*arr[j])/(int)pow(2,2*k))%2);
-                ans += (val1*val2);
+                int val1 = pow(2,k)*((arr[i]/(int)pow(2,k))%2);
+                int val2 = (arr[j]/(int)pow(2,k))%2;
+                ans+=(val1*val2);
                 // cout<<val1<<" "<<val2<<endl;
                 if(arr[i]/(int)pow(2,k)==0 && arr[j]/(int)pow(2,k) ==0)
                     break;
@@ -29,8 +31,8 @@ ll findValue(int n,int arr[]){
 }
 
 int main(){
-    int n=2;
-    int arr[]={11,39};
+    int n=6;
+    int arr[]={34,48,8,72,67,90};
     cout<<findValue(n,arr);
     return 0;
 }
