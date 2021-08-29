b9d3067855f5bdff0101f9c9d145d7c9fffdef1e
AbhinavKoul <47354541+AbhinavKoul@users.noreply.github.com>
2021-08-29 11:16:43 +0530

    final bitwise

--- Online Coding Exams/Sprinkler/function.cpp
+++ Online Coding Exams/Sprinkler/function.cpp
@@ -6,22 +6,57 @@
 #include<string>
 #include<algorithm>
 #include<set>
+#include<bitset>
+#include<math.h>
 using namespace std;
-#define v vector
-using namespace std;
-
+#define mod 1000000009
 typedef long long ll;
+#define v vector
+
+int maxSetBit(int n)
+{
+    return (int)(log2(n));
+}
+ll findValueBitwise(v<ll> arr)
+{
+    int n = arr.size();
+    ll maximum = *max_element(arr.begin(),arr.end());
+    int k = maxSetBit(maximum);
+
+    ll ans = 0;
+    for(int bit = 0;bit<=k;bit++)
+    {
+        for(int i = 0;i<n-1;i++)
+        {
+            bitset<20> val1(arr[i]);
+            if(!val1.test(bit))
+                continue;
+            
+            for(int j = i+1;j<n;j++)
+            {
+                bitset<20> val2(arr[j]);
+
+                if(!val2.test(bit))
+                    continue;
+                
+                ans += (1<<bit);
+            }
+        }
+    }
+
+    return ans;
+}
 
 ll findValue(int n,int arr[]){
     long long ans=0;
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             for(int k=0;k<=1e3;k++){
-                int val1 = pow(2,k)*((arr[i]/(int)pow(2,k))%2);
-                int val2 = (arr[j]/(int)pow(2,k))%2;
-                ans+=(val1*val2);
+                int val1 = ((arr[i]>>k)%2)<<k;
+                int val2 = (arr[j]>>k)%2;
+                ans += (val1*val2);
                 // cout<<val1<<" "<<val2<<endl;
-                if(arr[i]/(int)pow(2,k)==0 && arr[j]/(int)pow(2,k) ==0)
+                if(arr[i]>>k==0 || arr[j]>>k==0)
                     break;
             }
         }
@@ -31,8 +66,9 @@ ll findValue(int n,int arr[]){
 }
 
 int main(){
-    int n=6;
-    int arr[]={34,48,8,72,67,90};
-    cout<<findValue(n,arr);
+    int n=3;
+    int arr[]={88,78};
+    v<ll> arr1 = {34,48,8,72,67,90};
+    cout<<"hello : "<<findValueBitwise(arr1);
     return 0;
 }
